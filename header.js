80478f33d310ecba931b2f712c04b8ce0886432f
antnish1 <shahdolho@gmail.com>
2026-03-30 01:42:04 +0530

    Update header.js

--- header.js
+++ header.js
@@ -3,10 +3,10 @@ function Header(user) {
     <div class="flex items-center justify-between p-4 border-b border-gray-700">
       <div>
         <p class="text-sm text-gray-400">Branch</p>
-        <p class="font-semibold text-yellow-400">${user.branch}</p>
+        <p class="font-semibold text-yellow-1400">${user.branch}</p>
       </div>
 
-      <button onclick="logout()" class="text-sm text-red-400">
+      <button onclick="logout()" class="text-sm text-red-800">
         Logout
       </button>
     </div>
