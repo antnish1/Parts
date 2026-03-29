db3188a75a69532ab3b9d429f915941af733abff
antnish1 <shahdolho@gmail.com>
2026-03-29 23:59:16 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,14 @@
+function Header(user) {
+  return `
+    <div class="flex items-center justify-between p-4 border-b border-gray-700">
+      <div>
+        <p class="text-sm text-gray-400">Branch</p>
+        <p class="font-semibold text-yellow-400">${user.branch}</p>
+      </div>
+
+      <button onclick="logout()" class="text-sm text-red-400">
+        Logout
+      </button>
+    </div>
+  `;
+}
