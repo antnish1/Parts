a9cb94bdfde7cbbbbd375311268275a225d42b2c
antnish1 <shahdolho@gmail.com>
2026-03-30 00:55:52 +0530

    Update partCard.js

--- partCard.js
+++ partCard.js
@@ -1,14 +1,15 @@
 function PartCard() {
   return `
-    <div class="bg-black p-4 rounded-2xl space-y-3 border border-gray-700">
+    <div class="bg-black p-4 rounded-2xl space-y-4 border border-gray-700">
 
-      <input class="partNo input" placeholder="Part No"/>
+      <div class="grid grid-cols-2 gap-3">
+        <input class="partNo input" placeholder="Part No"/>
+        <input type="number" class="qty input" placeholder="Qty"/>
+      </div>
 
-      <input type="number" class="qty input" placeholder="Quantity"/>
+      <div class="text-base text-gray-300 desc">Description</div>
 
-      <div class="text-sm text-gray-400 desc">Description</div>
-
-      <div class="flex justify-between text-sm">
+      <div class="flex justify-between text-base">
         <span class="text-yellow-400 dnp">₹0</span>
         <span class="text-green-400 value">₹0</span>
       </div>
