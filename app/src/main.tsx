42bda44f2d588659d15d718096166d2e5e40e935
antnish1 <shahdolho@gmail.com>
2026-06-30 14:13:22 +0530

    Remove missing CSS import

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { queryClient } from './lib/queryClient';
-import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
