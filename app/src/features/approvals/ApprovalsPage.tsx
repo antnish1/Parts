6485c038142e2f73077378a71991263488ab2581
antnish1 <shahdolho@gmail.com>
2026-06-30 14:11:29 +0530

    Add approvals page scaffold

---
+++ app/src/features/approvals/ApprovalsPage.tsx
@@ -0,0 +1,5 @@
+import { PageCard } from '../../components/ui/PageCard';
+
+export function ApprovalsPage() {
+  return <PageCard eyebrow="Approvals" title="Approval Queue" description="Approval module placeholder for the React rebuild." />;
+}
