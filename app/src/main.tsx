2bb4e590456d6c894a99ac6891d31d02a5dd6ec6
antnish1 <shahdolho@gmail.com>
2026-06-30 18:26:11 +0530

    Load CSS in React entry

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { queryClient } from './lib/queryClient';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
