7ba5225fcfd5d30fb633dbccba38339a16ab07ba
antnish1 <shahdolho@gmail.com>
2026-07-06 21:17:41 +0530

    Remove obsolete staging migration file

--- supabase/migrations/004_add_test_admin_processing_fields.sql
+++
@@ -1,6 +0,0 @@
-alter table public.test_orders
-add column if not exists processing_reference text,
-add column if not exists processed_notes text;
-
-create index if not exists idx_test_orders_processing_reference
-on public.test_orders(processing_reference);
