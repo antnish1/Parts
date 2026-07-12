e2ebd737d0fafabd81652867a7251319d683795a
antnish1 <shahdolho@gmail.com>
2026-07-13 00:47:26 +0530

    Load desktop sidebar contrast overrides

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -11,6 +11,7 @@ import './responsive-table-cards.css';
 import './desktop-foundation.css';
 import './desktop-core-pages.css';
 import './mobile-approval-contrast.css';
+import './desktop-sidebar-contrast.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
